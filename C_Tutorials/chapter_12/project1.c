9624ec059ca0a3201cc1aa0e1349d193b9d87195
Jason Kwon <jasonykwon91@gmail.com>
2020-03-07 13:29:12 -0600

    chapter 12 C: modern programming language

---
+++ C_Tutorials/chapter_12/project1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define MSG_LEN 80	/* maximum length of message */
+
+int main(void)
+{
+	char msg[MSG_LEN];
+	int i;
+
+	printf("Enter a message: ");
+	for (i = 0; i < MSG_LEN; i++) {
+		msg[i] = getchar();
+		if (msg[i] == '\n')
+			break;
+	}
+
+	printf("Reversal is: ");
+	for (i--; i >= 0; i--)
+		putchar(msg[i]);
+	putchar('\n');
+
+	return 0;
+}
