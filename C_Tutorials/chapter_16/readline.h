c013c57f6d46c6e41877281a4b950a10cf24fc6a
Jason Kwon <jasonykwon91@gmail.com>
2020-03-09 10:13:41 -0500

    Chapter 16 C: Modern programming languae

---
+++ C_Tutorials/chapter_16/readline.h
@@ -0,0 +1,14 @@
+#ifndef READLINE_H
+#define READLINE_H
+
+
+ /******************************************************
+  *read_line:	skips leading white-space characters, then	*
+  		reads the remainder of the input line and	*
+		stores it in str. truncates the line if its	*
+		length exceeds n. returns the number of		*
+		characters stored.
+ *******************************************************/
+int read_line(char str[], int n);
+
+#endif
